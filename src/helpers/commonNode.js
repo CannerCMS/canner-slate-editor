4b88cb3286998350a274a3fb06fc9e7c96e975dd
howardchi <howardchi@fougood.com>
2016-12-12 17:39:53 +0800

    bump fix list

--- src/helpers/commonNode.js
+++ src/helpers/commonNode.js
@@ -5,11 +5,19 @@ export default function(Tag) {
   const NodeComponent = ({attributes, children, node}) => {
     const align = node.data.get('align');
     const indent = node.data.get('indent') || 0;
+    let style;
+
+    if (Tag === 'ul' || Tag === 'ol') {
+      style = {textAlign: align};
+    } else {
+      style = {textAlign: align, paddingLeft: `${3 * indent}em`};
+    }
+
     return (
       <Tag
         {...attributes}
         data-slate-type={Tag}
-        style={{textAlign: align, paddingLeft: `${3 * indent}em`}}
+        style={style}
       >{children}</Tag>
     );
   };
