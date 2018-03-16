5115ee32cd1d328ff8314a95946d8a90db96c2ef
chilijung <chilijung@gmail.com>
2018-03-16 11:24:12 +0800

    add emoji

---
+++ packages/slate-icon-emoji/src/index.js
@@ -0,0 +1,41 @@
+// @flow
+import * as React from 'react';
+import type {IconProps} from 'shared/src/types';
+import EmojiMartPicker from 'emoji-mart-picker';
+import ToolbarIcon from '@canner/slate-icon-shared';
+
+export default class Emoji extends React.Component<IconProps> {
+  typeName: string
+  constructor(props: IconProps) {
+    super(props);
+    this.typeName = this.props.type || 'emoji';
+  }
+
+  onChange = (emoji: Object) => {
+    let {change, onChange} = this.props;
+    onChange(
+      change
+        .insertInline({
+          type: this.displayName,
+          isVoid: true,
+          data: {emoji}
+        })
+    );
+  }
+
+  render() {
+    const {icon, ...rest} = this.props;
+
+    return (
+      <EmojiMartPicker
+        onChange={this.onChange}>
+        <ToolbarIcon
+          type={this.displayName}
+          icon={icon || 'Emoji'}
+          isActive={false}
+          {...rest}
+        />
+      </EmojiMartPicker>
+    );
+  }
+}
