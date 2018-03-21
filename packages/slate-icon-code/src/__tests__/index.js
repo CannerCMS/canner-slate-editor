dfc3fce9797977d4093a83f90b8c36bbecf2389c
chilijung <chilijung@gmail.com>
2018-03-21 15:53:34 +0800

    add code test

--- packages/slate-icon-code/src/__tests__/index.js
+++ packages/slate-icon-code/src/__tests__/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Change, Value } from "slate";
+import { join } from "path";
 import Code from "../";
 import renderer from "react-test-renderer";
+import iconTest from "../../../../test/icon-test";
 
 test("create a Code icon", () => {
   const initialValue = Value.fromJSON({
@@ -31,4 +33,8 @@ test("create a Code icon", () => {
 
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+test("simulate click icon", done => {
+  iconTest(Code, join(__dirname, "expected.yaml"), done);
 });
