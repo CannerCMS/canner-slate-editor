91e95dc5d190eeb87af8eb418172e5b8af870134
chilijung <chilijung@gmail.com>
2018-04-02 10:52:34 +0800

    add simulate click in align

--- packages/slate-icon-align/src/__tests__/index.js
+++ packages/slate-icon-align/src/__tests__/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { Change, Value } from "slate";
+import { join } from "path";
 import { AlignCenter, AlignLeft, AlignRight } from "../";
+import iconTest from 'test/icon-test';
 import renderer from "react-test-renderer";
 const onChange = arg => arg;
 
 test("create a AlignCenter icon", () => {
   const component = renderer.create(
-    <AlignCenter change={new Change({ value: new Value() })} onChange={onChange}/>
+    <AlignCenter
+      change={new Change({ value: new Value() })}
+      onChange={onChange}
+    />
   );
 
   let tree = component.toJSON();
@@ -15,7 +20,10 @@ test("create a AlignCenter icon", () => {
 
 test("create a AlignLeft icon", () => {
   const component = renderer.create(
-    <AlignLeft change={new Change({ value: new Value() })} onChange={onChange} />
+    <AlignLeft
+      change={new Change({ value: new Value() })}
+      onChange={onChange}
+    />
   );
 
   let tree = component.toJSON();
@@ -24,9 +32,63 @@ test("create a AlignLeft icon", () => {
 
 test("create a AlignRight icon", () => {
   const component = renderer.create(
-    <AlignRight change={new Change({ value: new Value() })} onChange={onChange} />
+    <AlignRight
+      change={new Change({ value: new Value() })}
+      onChange={onChange}
+    />
   );
 
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+test("click AlignCenter icon", done => {
+  iconTest({
+    icon: AlignCenter,
+    expectedPath: join(__dirname, "expected-center.yaml"),
+    callback: done
+  })
+})
+
+test("click AlignCenter icon use customized type", done => {
+  iconTest({
+    icon: AlignCenter,
+    type: "customizeAlign",
+    expectedPath: join(__dirname, "custom-expected-center.yaml"),
+    callback: done
+  })
+})
+
+test("click AlignLeft icon", done => {
+  iconTest({
+    icon: AlignLeft,
+    expectedPath: join(__dirname, "expected-left.yaml"),
+    callback: done
+  })
+})
+
+test("click AlignLeft icon use customized type", done => {
+  iconTest({
+    icon: AlignLeft,
+    type: "customizeAlign",
+    expectedPath: join(__dirname, "custom-expected-left.yaml"),
+    callback: done
+  })
+})
+
+test("click AlignRight icon", done => {
+  iconTest({
+    icon: AlignRight,
+    expectedPath: join(__dirname, "expected-right.yaml"),
+    callback: done
+  })
+})
+
+test("click AlignRight icon use customized type", done => {
+  iconTest({
+    icon: AlignRight,
+    type: "customizeAlign",
+    expectedPath: join(__dirname, "custom-expected-right.yaml"),
+    callback: done
+  })
+})
