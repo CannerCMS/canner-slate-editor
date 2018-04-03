0d4dfe4c3d7c9838cd08d03f15ed2c10dcbe49ff
chilijung <chilijung@gmail.com>
2018-04-03 10:11:38 +0800

    refactor video

--- packages/slate-editor-renderer/src/components/videoModal.js
+++ packages/slate-editor-renderer/src/components/videoModal.js
@@ -1,8 +1,8 @@
 // @flow
-import * as React from 'react';
-import videoParser from 'js-video-url-parser';
-import type {Change} from 'slate';
-import {Modal, Form, Button, Input} from 'antd';
+import * as React from "react";
+import videoParser from "js-video-url-parser";
+import type { Change } from "slate";
+import { Modal, Form, Button, Input } from "antd";
 
 const FormItem = Form.Item;
 
@@ -25,33 +25,48 @@ type Props = {
 
 @Form.create()
 export default class VideoModal extends React.Component<Props> {
-
   checkSource(rule: string, value: string, callback: any) {
     try {
       const videoObj = videoParser.parse(value);
 
-      if (videoObj && videoObj.provider === 'youtube' ||
-        videoObj.provider === 'dailymotion' ||
-        videoObj.provider === 'vimeo' ||
-        videoObj.provider === 'youku') {
+      if (
+        (videoObj && videoObj.provider === "youtube") ||
+        videoObj.provider === "dailymotion" ||
+        videoObj.provider === "vimeo" ||
+        videoObj.provider === "youku"
+      ) {
         callback();
         return;
       }
 
-      return callback('URL not support');
+      return callback("URL not support");
     } catch (e) {
-      return callback('URL not support');
+      return callback("URL not support");
     }
   }
 
   handleCancel = () => {
     this.props.form.resetFields();
     this.props.hideModal();
-  }
+  };
 
   handleOk = (e: Event) => {
     e.preventDefault();
-    const {onChange, change, hideModal, form, initialValue, node, width, height, youtubeType, dailymotionType, youkuType, vimeoType, idKey} = this.props;
+    const {
+      onChange,
+      change,
+      hideModal,
+      form,
+      initialValue,
+      node,
+      width,
+      height,
+      youtubeType,
+      dailymotionType,
+      youkuType,
+      vimeoType,
+      idKey
+    } = this.props;
 
     form.validateFields((err, values) => {
       if (!err) {
@@ -60,22 +75,38 @@ export default class VideoModal extends React.Component<Props> {
           const videoObj = videoParser.parse(href);
           let slateObj: Object = {};
 
-          if (videoObj && videoObj.provider === 'youtube') {
-            slateObj = {type: youtubeType, isVoid: true, data: {[idKey]: videoObj.id}};
-          } else if (videoObj && videoObj.provider === 'dailymotion') {
-            slateObj = {type: dailymotionType, isVoid: true, data: {[idKey]: videoObj.id}};
-          } else if (videoObj && videoObj.provider === 'vimeo') {
-            slateObj = {type: youkuType, isVoid: true, data: {[idKey]: videoObj.id}};
-          } else if (videoObj && videoObj.provider === 'youku') {
-            slateObj = {type: vimeoType, isVoid: true, data: {[idKey]: videoObj.id}};
+          if (videoObj && videoObj.provider === "youtube") {
+            slateObj = {
+              type: youtubeType,
+              isVoid: true,
+              data: { [idKey]: videoObj.id }
+            };
+          } else if (videoObj && videoObj.provider === "dailymotion") {
+            slateObj = {
+              type: dailymotionType,
+              isVoid: true,
+              data: { [idKey]: videoObj.id }
+            };
+          } else if (videoObj && videoObj.provider === "vimeo") {
+            slateObj = {
+              type: youkuType,
+              isVoid: true,
+              data: { [idKey]: videoObj.id }
+            };
+          } else if (videoObj && videoObj.provider === "youku") {
+            slateObj = {
+              type: vimeoType,
+              isVoid: true,
+              data: { [idKey]: videoObj.id }
+            };
           }
 
           if (slateObj && slateObj.data) {
-            if (typeof slateObj.data === 'object' && width) {
+            if (typeof slateObj.data === "object" && width) {
               slateObj.data.width = width;
             }
 
-            if (typeof slateObj.data === 'object' && height) {
+            if (typeof slateObj.data === "object" && height) {
               slateObj.data.height = height;
             }
           }
@@ -97,21 +128,31 @@ export default class VideoModal extends React.Component<Props> {
         hideModal();
       }
     });
-  }
+  };
 
   render() {
-    const {getFieldDecorator} = this.props.form;
-    const {isShow, initialValue} = this.props;
+    const { getFieldDecorator } = this.props.form;
+    const { isShow, initialValue } = this.props;
     return (
       <Modal
         visible={isShow}
         title="Add Video"
         onCancel={this.handleCancel}
         footer={[
-          <Button key="back" type="ghost" size="large" onClick={this.handleCancel}>
+          <Button
+            key="back"
+            type="ghost"
+            size="large"
+            onClick={this.handleCancel}
+          >
             Cancel
           </Button>,
-          <Button key="submit" type="primary" size="large" onClick={this.handleOk}>
+          <Button
+            key="submit"
+            type="primary"
+            size="large"
+            onClick={this.handleOk}
+          >
             Ok
           </Button>
         ]}
@@ -121,18 +162,22 @@ export default class VideoModal extends React.Component<Props> {
             label="Enter video URL (support Vimeo, Youtube, Dailymotion, Youku):"
             hasFeedback
           >
-            {getFieldDecorator('href', {
-              rules: [{
-                type: 'url', message: 'The input is not valid url!'
-              }, {
-                required: true, message: 'Please input your url'
-              }, {
-                validator: this.checkSource
-              }],
+            {getFieldDecorator("href", {
+              rules: [
+                {
+                  type: "url",
+                  message: "The input is not valid url!"
+                },
+                {
+                  required: true,
+                  message: "Please input your url"
+                },
+                {
+                  validator: this.checkSource
+                }
+              ],
               initialValue: initialValue
-            })(
-              <Input onClick={e => e.preventDefault()}/>
-            )}
+            })(<Input onClick={e => e.preventDefault()} />)}
           </FormItem>
         </Form>
       </Modal>
