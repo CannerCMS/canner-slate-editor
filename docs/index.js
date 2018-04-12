c0699ec7d548ffddf373337262882cdaf4289368
chilijung <chilijung@gmail.com>
2018-04-12 14:23:15 +0800

    update demo

--- docs/index.js
+++ docs/index.js
@@ -2,9 +2,10 @@
 /* eslint-disable no-console */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import type {Value} from 'slate';
 import {Editor} from 'slate-react';
 import beautify from 'js-beautify';
-import {Row, Col} from 'antd';
+import {Row, Col, Tabs} from 'antd';
 import {AlignCenter, AlignLeft, AlignRight, AlignCenterPlugin, AlignLeftPlugin, AlignRightPlugin} from 'packages/slate-icon-align';
 import Blockquote, {BlockquotePlugin} from 'packages/slate-icon-blockquote';
 import Bold, {BoldPlugin} from 'packages/slate-icon-bold';
@@ -55,6 +56,7 @@ import Html from 'slate-html-serializer';
 import {DEFAULT_RULES} from 'packages/slate-editor-html';
 
 const html = new Html({ rules: DEFAULT_RULES})
+const TabPane = Tabs.TabPane;
 
 import "./style.css";
 import "./github-markdown.css";
@@ -136,76 +138,94 @@ const plugins = [
   VideoPlugin()
 ];
 
+class SerializeHTML extends React.Component<{value: Value}> {
+  componentDidMount() {
+    Prism.highlightAllUnder(document.getElementById('root'));
+  }
+
+  render() {
+    const {value} = this.props;
+    const htmlValue = html.serialize(value);
+    const dataObj = html.deserialize(htmlValue);
+    const beautyHTML = beautify.html(htmlValue, { indent_size: 2, space_in_empty_paren: true })
+    console.log('--------------Deserialize from HTML--------------')
+    console.log(dataObj.toJSON())
+
+    return (
+      <Col style={{padding: '5px 0 5px 10px'}}>
+        <h3>Serialized HTML</h3>
+        <pre>
+          <code className="language-markup">
+            {beautyHTML}
+          </code>
+        </pre>
+      </Col>
+    )
+  }
+}
+
 class App extends React.Component {
   // Set the initial state when the app is first constructed.
   state = {
     value: initialValue
   }
 
-  componentDidUpdate() {
-    Prism.highlightAllUnder(document.getElementById('root'));
-  }
-
   render() {
     const {value} = this.state;
     const onChange = ({value}) => this.setState({value});
-    const htmlValue = html.serialize(value);
-    const dataObj = html.deserialize(htmlValue);
-    const beautyHTML = beautify.html(htmlValue, { indent_size: 2, space_in_empty_paren: true })
 
     console.log('--------------Current Value----------------')
     console.log(value.toJSON())
-    console.log('--------------Deserialize from HTML--------------')
-    console.log(dataObj.toJSON())
 
     return (
-      <Row>
-        <Col span={12} style={{borderRight: '1px solid #DDD', minHeight: '100vh'}}>
-          <div className="toolbar">
-            <div>
-              {selectors.map((Type, i) => {
-                return <Type
-                  change={value.change()}
+      <Tabs>
+        <TabPane tab="editor" key="editor">
+          <Row>
+            <Col style={{borderRight: '1px solid #DDD', minHeight: '100vh'}}>
+              <div className="toolbar">
+                <div>
+                  {selectors.map((Type, i) => {
+                    return <Type
+                      change={value.change()}
+                      onChange={onChange}
+                      key={i}
+                      className="toolbar-select"
+                    />
+                  })}
+                </div>
+                <div>
+                  {icons.map((Type, i) => {
+                    return <Type
+                      change={value.change()}
+                      onChange={onChange}
+                      key={i}
+                      className="toolbar-item"
+                      activeClassName="toolbar-item-active"
+                      disableClassName="toolbar-item-disable"
+                      activeStrokeClassName="ql-stroke-active"
+                      activeFillClassName="ql-fill-active"
+                      activeThinClassName="ql-thin-active"
+                      activeEvenClassName="ql-even-active"
+                    />
+                  })}
+                </div>
+              </div>
+              <div className="editor markdown-body">
+                <Editor
+                  value={value}
                   onChange={onChange}
-                  key={i}
-                  className="toolbar-select"
+                  plugins={plugins}
                 />
-              })}
-            </div>
-            <div>
-              {icons.map((Type, i) => {
-                return <Type
-                  change={value.change()}
-                  onChange={onChange}
-                  key={i}
-                  className="toolbar-item"
-                  activeClassName="toolbar-item-active"
-                  disableClassName="toolbar-item-disable"
-                  activeStrokeClassName="ql-stroke-active"
-                  activeFillClassName="ql-fill-active"
-                  activeThinClassName="ql-thin-active"
-                  activeEvenClassName="ql-even-active"
-                />
-              })}
-            </div>
-          </div>
-          <div className="editor markdown-body">
-            <Editor
-              value={value}
-              onChange={onChange}
-              plugins={plugins}
-            />
-          </div>
-        </Col>
-        <Col span={12} style={{padding: '5px 0 5px 10px'}}>
-          <h3>Serialized HTML</h3>
-          <pre>
-            <code className="language-markup">
-              {beautyHTML}
-            </code>
-          </pre>
-        </Col>
-      </Row>
+              </div>
+            </Col>
+          </Row>
+        </TabPane>
+        <TabPane tab="HTML" key="html">
+          <Row>
+            <SerializeHTML value={value}/>
+          </Row>
+        </TabPane>
+      </Tabs>
     );
   }
 }
