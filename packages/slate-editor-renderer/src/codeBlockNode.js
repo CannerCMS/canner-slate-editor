2980aa55908c0a86fdaf0375ce23cf1194281f24
chilijung <chilijung@gmail.com>
2018-04-12 14:34:54 +0800

    remove no needs

--- packages/slate-editor-renderer/src/codeBlockNode.js
+++ packages/slate-editor-renderer/src/codeBlockNode.js
@@ -2,7 +2,6 @@
 import * as React from 'react';
 import type {nodeProps} from './type';
 import styled from 'styled-components';
-import {Icon, Dropdown} from 'antd';
 
 const CodeblockContainer = styled.div`
   position: relative;
