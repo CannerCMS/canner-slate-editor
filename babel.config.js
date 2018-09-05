05ec09119e0e1047604dab881a176c7e16630fd1
chilijung <chilijung@gmail.com>
2018-09-05 15:52:21 +0800

    update babel config

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          browsers: [">0.25%", "not ie 11", "not op_mini all"]
+        }
+      }
+    ],
+    "@babel/preset-react",
+    "@babel/preset-flow"
+  ],
+  plugins: [
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: false }]
+  ],
+  babelrcRoots: "packages/*"
+};
