3fddcf64dd4b4729f9b7a816c40d59d18d8a87f2
chilijung <chilijung@gmail.com>
2018-09-07 17:50:35 +0800

    update storybook link

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,15 @@
 import { configure } from "@storybook/react";
+import { setOptions } from "@storybook/addon-options";
 
 // automatically import all files ending in *.stories.js
 const req = require.context("../stories", true, /.stories.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+setOptions({
+  name: "canner-slate-editor",
+  url: "https://github.com/Canner/canner-slate-editor"
+});
 
 configure(loadStories, module);
